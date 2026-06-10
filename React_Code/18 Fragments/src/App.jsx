f42b9d68b103dfdc374d977f99d50d88258cac79
RaushanDevloper <raushankumarsingh7033@gmail.com>
2026-06-10 23:12:11 +0530

    change code

--- React_Code/18 Fragments/src/App.jsx
+++ React_Code/18 Fragments/src/App.jsx
@@ -1,5 +1,6 @@
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
@@ -8,12 +9,20 @@ function App() {
   // let foodItems = [];
   let foodItems = ["sabzi","Green Vegetable","Roti","Salad","Milk","Ghee"];
   return (
-  <React.Fragment>
+    <>
+  <Container>
   <h1 className="food-heading">Healthy Food</h1>
   <ErrorMessage items={foodItems}></ErrorMessage>
     {/* {foodItems.length === 0 && <h3>I am still hungry.</h3>} */}
    <FoodItems items={foodItems}></FoodItems>
-  </React.Fragment>
+  </Container>
+
+  {/* <Container>
+    <p>
+      Above is the list of healthy foods that are good for your health and well being.
+    </p>
+  </Container> */}
+  </>
   );
 }
 
--- React_Code/18 Fragments/src/components/Item.jsx
+++ React_Code/18 Fragments/src/components/Item.jsx
@@ -1,14 +1,21 @@
 import styles from "./Item.module.css";
 
 const Item = ({ foodItem }) => {
-   console.log(Object.keys(styles));
+  const handleBuyButtonClicked = () => {
+    console.log(`${foodItem} being bought `);
+  };
+
+
   return ( 
-    <li className={`${styles["kg-item"]}`}>
+    <li className={`${styles["kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>
         {foodItem}
       </span>
+      <button className={`${styles.button} btn btn-info`}
+      onClick={handleBuyButtonClicked}
+      >Buy</button>
     </li>
-  );
+  ); 
 };
 
 export default Item;
